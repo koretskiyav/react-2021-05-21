210eacfa37016874be6af91f58e5e6b166e81ac8
Alex Sin <lighting13@yandex.ru>
2021-05-26 20:18:04 +0300

    last commit

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -1,32 +1,36 @@
-import { useEffect, useState } from "react";
-import Rate from "./rate";
-import style from './reviews.module.css'
+import { useEffect, useState } from 'react';
+import Rate from './rate';
+import style from './reviews.module.css';
 function Reviews(props) {
-  const [sumRate,setSumRate] = useState(0)
+  const [sumRate, setSumRate] = useState(0);
 
   // Вычисляем среднюю арифметическую по рейтингу
-  useEffect(()=> {
-    setSumRate(((props.reviews.reduce((sum,current)=> sum + current.rating, 0)) / props.reviews.length).toFixed(2));
-  },[props.reviews]);
+  useEffect(() => {
+    setSumRate(
+      (
+        props.reviews.reduce((sum, current) => sum + current.rating, 0) /
+        props.reviews.length
+      ).toFixed(2)
+    );
+  }, [props.reviews]);
 
-  // Мапим все отзывы 
-  const reviewsList = props.reviews.map((review) => { 
+  // Мапим все отзывы
+  const reviewsList = props.reviews.map((review) => {
     return (
       <div className={style.card} key={review.id}>
         <h3>{review.user}</h3>
         <p className={style.comment}>{review.text}</p>
-        <div><Rate rating={review.rating}/></div>
+        <div>
+          <Rate rating={review.rating} />
+        </div>
       </div>
-      )
-    }
-  );
+    );
+  });
 
   return (
-    <div >
-      <Rate rating={sumRate}/>
-      <div>
-        {reviewsList}
-      </div>
+    <div>
+      <Rate rating={sumRate} />
+      <div>{reviewsList}</div>
     </div>
   );
 }
