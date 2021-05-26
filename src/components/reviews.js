65ef2d603a0a817c205b16a07231d3a482dab1c1
Alex Sin <lighting13@yandex.ru>
2021-05-26 20:05:57 +0300

    Refactor

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -1,28 +1,32 @@
 import { useEffect, useState } from "react";
 import Rate from "./rate";
-
+import style from './reviews.module.css'
 function Reviews(props) {
   const [sumRate,setSumRate] = useState(0)
 
+  // Вычисляем среднюю арифметическую по рейтингу
   useEffect(()=> {
-    setSumRate(((props.reviews.reduce((sum,current)=> sum + current.rating, 0)) / props.reviews.length).toFixed(2))
+    setSumRate(((props.reviews.reduce((sum,current)=> sum + current.rating, 0)) / props.reviews.length).toFixed(2));
   },[props.reviews]);
 
+  // Мапим все отзывы 
   const reviewsList = props.reviews.map((review) => { 
     return (
-      <div key={review.id}>
-        <p>{review.user}</p>
-        <p>{review.text}</p>
-        <p><Rate rating={review.rating}/></p>
+      <div className={style.card} key={review.id}>
+        <h3>{review.user}</h3>
+        <p className={style.comment}>{review.text}</p>
+        <div><Rate rating={review.rating}/></div>
       </div>
       )
     }
   );
 
   return (
-    <div>
+    <div >
       <Rate rating={sumRate}/>
-      {reviewsList}
+      <div>
+        {reviewsList}
+      </div>
     </div>
   );
 }
