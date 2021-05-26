3f505536e7a6f0971f34dd1017f121e2a54f60a6
16884117 <Kavalerova.K.Alb@sberbank.ru>
2021-05-26 18:03:07 +0300

    заменила на картинку

--- src/components/rate.js
+++ src/components/rate.js
@@ -1,8 +1,15 @@
 import { ReactComponent as Star } from '../icons/star.svg';
 
-export default function Rate(props){
+export default function Rate(props) {
 
- while(props.value){
-   return <Star/>
- }
+  const StarItems = () => {
+    for (let index = 0; index < props.value; index++) {
+    return <Star key={index} width={30} height={30}/>
+  }}
+
+  return (
+    <span>
+      {StarItems}
+    </span>
+  );
 }
