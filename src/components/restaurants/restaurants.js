e6dac4dbf85c6321137912fe038d8a11cdf0d2b8
achat <achat@custis.ru>
2021-05-31 15:53:54 +0300

    Задание №2

--- src/components/restaurants/restaurants.js
+++ src/components/restaurants/restaurants.js
@@ -1,6 +1,7 @@
 import { useState, useMemo } from 'react';
 import Restaurant from '../restaurant';
 import Navigation from '../navigation';
+import PropTypes from "prop-types";
 
 const Restaurants = ({ restaurants }) => {
   const [activeId, setActiveId] = useState(restaurants[0].id);
@@ -16,6 +17,14 @@ const Restaurants = ({ restaurants }) => {
       <Restaurant restaurant={activeRestaurant} />
     </div>
   );
+};
+
+Restaurants.propTypes = {
+    restaurants: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
 };
 
 export default Restaurants;
