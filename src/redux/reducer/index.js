20578ade244d425c49ef00be044607cf8c8b0755
16884117 <Kavalerova.K.Alb@sberbank.ru>
2021-06-02 18:08:06 +0300

    H3

--- src/redux/reducer/index.js
+++ src/redux/reducer/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 import orderReducer from './order';
+import restaurantsReducer from "./restaurants";
+
+
 
 export default combineReducers({
   order: orderReducer,
+  restaurants: restaurantsReducer,
   foo: () => 'bar',
 });
