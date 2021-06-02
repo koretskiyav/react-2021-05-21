ffdbb1fe2779d402f0056073799ec9698e5a5039
Lipokurto <64575103+Lipokurto@users.noreply.github.com>
2021-06-02 10:56:41 +0300

    Basket Component + noMinusProduct

---
+++ src/components/Basket/basket.js
@@ -0,0 +1,4 @@
+const Basket = (props) => {
+  return <div>Корзина</div>;
+};
+export default Basket;
--- src/redux/reducer/order.js
+++ src/redux/reducer/order.js
@@ -5,9 +5,15 @@ export default (state = 0, action) => {
   const { type, id } = action;
   switch (type) {
     case INCREMENT:
-      return { ...state, [id]: (state[id] || 0) + 1 };
+      return {
+        ...state,
+        [id]: (state[id] || 0) + 1,
+      };
     case DECREMENT:
-      return { ...state, [id]: (state[id] || 0) - 1 };
+      return {
+        ...state,
+        [id]: state[id] > 0 ? state[id] - 1 : (state[id] = 0),
+      };
     default:
       return state;
   }
