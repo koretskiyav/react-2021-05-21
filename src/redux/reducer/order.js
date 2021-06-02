3d18eada80ade9e28094aa477eb8a3dc0d82f5f3
ignatov <ignatovdan@gmail.com>
2021-06-02 21:52:16 +0300

    'Order' object is the same instance until its property is no modified

--- src/redux/reducer/order.js
+++ src/redux/reducer/order.js
@@ -1,20 +1,17 @@
 import { DECREMENT, INCREMENT, REMOVE } from '../constants';
 
-function increment(value) {
-  return (value || 0) + 1;
-}
 // { [productId]: amount }
 export default (state = 0, action) => {
   const { type, id } = action;
   switch (type) {
     case INCREMENT:
-      return { ...state, [id]: (state[id] || 0) + 1, version: increment(state.version) };
+      return { ...state, [id]: (state[id] || 0) + 1 };
     case DECREMENT:
-      return { ...state, [id]: (state[id] || 0) - 1, version: increment(state.version) };
+      return { ...state, [id]: (state[id] || 0) - 1 };
     case REMOVE:
       const stateCopy = { ...state };
       delete stateCopy[id];
-      return { ...stateCopy, version: increment(state.version) };
+      return { ...stateCopy };
     default:
       return state;
   }
