b465b6e13588a564b588b09fef3d9003a80dbd1b
LeXik0N <lexik0n1993@yandex.ru>
2021-06-11 01:39:11 +0600

    syntactic fix in review

--- src/components/reviews/review/review.js
+++ src/components/reviews/review/review.js
@@ -5,25 +5,23 @@ import Rate from '../../rate';
 import styles from './review.module.css';
 import { reviewWitUserSelector } from '../../../redux/selectors';
 
-const Review = ({ user, text, rating }) => {
-  return (
-    <div className={styles.review} data-id="review">
-      <div className={styles.content}>
-        <div>
-          <h4 className={styles.name} data-id="review-user">
-            {user}
-          </h4>
-          <p className={styles.comment} data-id="review-text">
-            {text}
-          </p>
-        </div>
-        <div className={styles.rate}>
-          <Rate value={rating} />
-        </div>
+const Review = ({ user, text, rating }) => (
+  <div className={styles.review} data-id="review">
+    <div className={styles.content}>
+      <div>
+        <h4 className={styles.name} data-id="review-user">
+          {user}
+        </h4>
+        <p className={styles.comment} data-id="review-text">
+          {text}
+        </p>
+      </div>
+      <div className={styles.rate}>
+        <Rate value={rating} />
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 Review.propTypes = {
   user: PropTypes.string,
