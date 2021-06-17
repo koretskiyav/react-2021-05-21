0f9a4c7b8c08ddd92141623d04a98c2d0fe47f52
LeXik0N <lexik0n1993@yandex.ru>
2021-06-18 02:58:28 +0600

    Homework 7

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Switch, Route } from 'react-router-dom';
 
 import styles from './basket.module.css';
 import itemStyles from './basket-item/basket-item.module.css';
@@ -9,9 +9,23 @@ import Button from '../button';
 import { orderProductsSelector, totalSelector } from '../../redux/selectors';
 import { UserConsumer } from '../../contexts/user';
 import moneyContext from '../../contexts/money';
+import { checkout, orderIsLoadingSelector } from '../../redux/features/order';
+import Loader from '../loader';
 
-function Basket({ title = 'Basket', total, orderProducts }) {
+function Basket({ title = 'Basket', total, orderProducts, checkout, loading }) {
   const { m } = useContext(moneyContext);
+
+  const submitHandler = () => {
+    checkout(orderProducts);
+  };
+
+  if (loading) {
+    return (
+      <div className={styles.basket}>
+        <Loader />
+      </div>
+    );
+  }
 
   if (!total) {
     return (
@@ -43,20 +57,30 @@ function Basket({ title = 'Basket', total, orderProducts }) {
           <p>{m(total)}</p>
         </div>
       </div>
-      <Link to="/checkout">
-        <Button primary block>
-          checkout
-        </Button>
-      </Link>
+      <Switch>
+        <Route path="/checkout">
+          <Button primary block onClick={submitHandler}>
+            checkout
+          </Button>
+        </Route>
+        <Route>
+          <Link to="/checkout">
+            <Button primary block>
+              checkout
+            </Button>
+          </Link>
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, props) => {
   return {
     total: totalSelector(state),
     orderProducts: orderProductsSelector(state),
+    loading: orderIsLoadingSelector(state, props),
   };
 };
 
-export default connect(mapStateToProps)(Basket);
+export default connect(mapStateToProps, { checkout })(Basket);
